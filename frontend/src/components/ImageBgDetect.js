b14177bc71960500f90e8a06614ca5818217a0fb
Om <omprxz@gmail.com>
2024-06-21 18:31:47 +0530

    c++ to cpp highliting

--- frontend/src/components/ImageBgDetect.js
+++ frontend/src/components/ImageBgDetect.js
@@ -2,8 +2,12 @@ import axios from 'axios';
 import { extractColors } from 'extract-colors';
 
 const ImageBgDetect = async (imagePath) => {
+  const api_baseurl =
+        window.location.hostname === "localhost"
+            ? process.env.REACT_APP_API_BASEURL_LOCAL
+            : process.env.REACT_APP_API_BASEURL_PRODUCTION;
     try {
-      const proxyUrl = `http://localhost:3300/api/proxy-image?url=${encodeURIComponent(imagePath)}`;
+      const proxyUrl = `${api_baseurl}/api/proxy-image?url=${encodeURIComponent(imagePath)}`;
       const response = await axios.get(proxyUrl, {
         responseType: 'arraybuffer'
       });
