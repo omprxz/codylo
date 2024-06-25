1532e26092a62440a9aaaafc7f2fb1917eb9ed66
Om <omprxz@gmail.com>
2024-06-25 17:19:56 +0530

    Added AOS Animations 6

--- backend/db/Feedback.js
+++ backend/db/Feedback.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 const timestamps = require('mongoose-timestamp');
-
-mongoose.plugin(timestamps, {
-  createdAt: 'created_at',
-  updatedAt: 'updated_at',
-  currentTime: () => new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})
-});
-
 const feedbackSchema = new mongoose.Schema({
   name: String,
   email: String,
   feedback: {type: String, maxlength: 1000}
-})
+}, { timestamps: true })
 
 const Feedback = mongoose.model('Feedback', feedbackSchema, 'feedbacks')
 
--- backend/db/Image2Code.js
+++ backend/db/Image2Code.js
@@ -1,19 +1,13 @@
 const mongoose = require('mongoose');
 const timestamps = require('mongoose-timestamp');
 
-mongoose.plugin(timestamps, {
-  createdAt: 'created_at',
-  updatedAt: 'updated_at',
-  currentTime: () => new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})
-});
-
 const image2codeSchema = new mongoose.Schema({
   imageurl: String,
   functionality: String,
   cssframework: String,
   usejquery: Boolean,
   ip: String
-})
+}, { timestamps: true })
 
 const Image2CodeModel = mongoose.model('Image2Code', image2codeSchema, 'image2code')
 
